8f2ff6e94b4f65c4727c2e352e09233fcc57e394
jeissonarcadio778 <jeissonbeltranporras@gmail.com>
2023-10-26 17:36:27 -0500

    27-10-23

---
+++ october/27-10-23.py
@@ -0,0 +1,20 @@
+"""
+Imagina que acabas de abrir una nueva cuenta de ahorros que te ofrece el 4% de interés al año. Estos ahorros debido a intereses, que no se cobran hasta finales de año, se te añaden al balance final de tu cuenta de ahorros. Escribir un programa que comience leyendo la cantidad de dinero depositada en la cuenta de ahorros, introducida por el usuario. Después el programa debe calcular y mostrar por pantalla la cantidad de ahorros tras el primer, segundo y tercer años. Redondear cada cantidad a dos decimales.
+"""
+
+def month_per_moth_interest(months:int, amount:float) -> None:
+    count = 1
+    while( months > count):
+        print (amount * ( 1 + 4/ 100) ** (count * (1/12)))
+        count += 1
+
+month_per_moth_interest(12, 100)
+
+
+"""
+Una panadería vende barras de pan a 3.49€ cada una. El pan que no es el día tiene un descuento del 60%. Escribir un programa que comience leyendo el número de barras vendidas que no son del día. Después el programa debe mostrar el precio habitual de una barra de pan, el descuento que se le hace por no ser fresca y el coste final total.
+"""
+
+
+
+
